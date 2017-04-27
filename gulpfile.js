93e49957548e245c688535b842d9edbd4c37dfa0
bartekmanu <zrobtodoitnow@gmail.com>
2017-04-28 00:08:47 +0200

    gulp, html

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
 
-
-gulp.task('scss', function() {
-    return gulp.src('sass/style.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass ({
-            errLogConsole: true,
-            outputStyle: 'nested',
-            sourceComments: false,
-        }).on("error", sass.logError))
-		.pipe(autoprefixer({
-				browsers:  [
-					'last 3 chrome versions',
-					'last 3 firefox versions',
-					'last 3 opera versions',
-					'last 3 safari versions',
-					'last 3 ios versions',
-					'android >= 4.0',
-					'ie >= 10' 	// change to `last 3 ie versions` after IE12  release.
-				]
-			}))
+gulp.task('gogulp',function(){
+	console.log("as you wish my master");
+});
+gulp.task('sass',function(){
+	return gulp.src('sass/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			errLogToConsole:true,
+			outputStyle:'expanded'
+		}))
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('css'))
-})
+	.pipe(gulp.dest('css'))
+});
 
-
-gulp.task('default', ['scss'], function() {
-    gulp.watch('sass/main.scss', ['scss'])
+gulp.task('watch',function(){
+	gulp.watch('sass/**/*.scss',['sass']);
 });
