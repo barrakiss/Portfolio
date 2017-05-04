11ccdb3f1bf5bdda5ac19cd5b6b18098be444a4e
bartekmanu <zrobtodoitnow@gmail.com>
2017-05-04 17:48:56 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 module.exports = {
-    entry: "./js/app.jsx",
-    output: { filename: "./js/out.js" },
-
-    devServer: {
-        inline: true,
-        contentBase: './',
-        port: 3001
-    },
-
-    watch: true,
-    module: {
-        loaders: [
-            {
-                test: /\.jsx$/,  exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: { presets: ['es2015', 'stage-2' , 'react'] }
-            }
-        ]
-    }
+  entry: ["./js/app.jsx"],
+  output: {filename: "./js/out.js"},
+  devServer: {
+    inline: true,
+    contentBase: './',
+    port: 3001
+  },
+  watch: true,
+  module: {
+    loaders:[
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['es2015','stage-2','react']
+        }
+      },
+      {
+        test:/\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  watch: true
 }
