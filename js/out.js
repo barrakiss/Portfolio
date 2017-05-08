d217d94aa467d1cb958a56d7e10ed99686713a4a
bartekmanu <zrobtodoitnow@gmail.com>
2017-05-08 22:19:48 +0200

    section2 done

--- js/out.js
+++ js/out.js
@@ -86,6 +86,16 @@ $(window).scroll(function () {
     if (bottom_of_screen > top_of_element && top_of_screen < bottom_of_element) {
         // The element is visible, do something
         $('.image1, .image3, .image5, .image7, .image9').removeClass('hide').addClass('right');
+    }
+    var top_of_element = $(".black_whole").offset().top;
+    var bottom_of_element = $(".black_whole").offset().top + $(".black_whole").outerHeight();
+    if (bottom_of_screen > top_of_element && top_of_screen < bottom_of_element) {
+        // The element is visible, do something
+        $('.about_me_text').removeClass('hide').addClass('animation_s1_1');
+    }
+    if (bottom_of_screen > top_of_element && top_of_screen < bottom_of_element) {
+        // The element is visible, do something
+        $('.about_me_photo').removeClass('hide').addClass('animation_s1_2');
     } else {
         // The element is not visible, do something else
     }
